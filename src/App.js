8477514fb87b5bf8e59fd814793f554a04b285c6
miltonejones <miltonejones@gmail.com>
2022-03-09 11:25:14 -0600

    fixing App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import SocketSender from './components/SocketSender';
-import JestCard from './components/JestCard';
+import SocketSender from './components/SocketSender'; 
 import Box from '@mui/material/Box';
 import './style.css';
 
 export default function App() {
-  return <JestCard />;
+  return <SocketSender />;
 }
