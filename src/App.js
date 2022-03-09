38a2961fcf856758ea7aa9eb28c09125e4042df4
miltonejones <miltonejones@gmail.com>
2022-03-09 10:39:53 -0600

    adding code vuiew

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SocketSender from './components/SocketSender';
+import JestCard from './components/JestCard';
 import Box from '@mui/material/Box';
 import './style.css';
 
 export default function App() {
-  return <SocketSender />;
+  return <JestCard />;
 }
--- src/components/JestCard.js
+++ src/components/JestCard.js
@@ -3,22 +3,46 @@ import { specimenText } from './Specimen';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import { TextField, Stack, Typography, Button, Divider } from '@mui/material';
+import { Box, Tab, Tabs,  TextField, Stack, Typography, Button, Divider } from '@mui/material';
 import { JestPuppeteerAdapter } from '../JestPuppeteerAdapter'
  
 
 const jest = new JestPuppeteerAdapter();
  
- 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default function JestCard ({ onSave, onCancel }) {
   const [puppetSteps, setPuppetSteps] = React.useState(null);
+  const [editMode, setEditMode] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   // load jest JS if none is present
-  if (!puppetSteps) {
+  if (!puppetSteps || editMode) {
     return <JestForm onClick={js => {
        jest.loadJest(js);
        setPuppetSteps(jest.tests);
+       setEditMode(false)
     }} onCancel={onCancel}/>
   }
  
@@ -29,24 +53,35 @@ export default function JestCard ({ onSave, onCancel }) {
     {Object.keys(puppetSteps).map(key => <>
 
       <Divider textAlign="left" style={{textTransform: 'capitalize', margin: 12}}>{key}</Divider>
-
-      <Stepper key={key} mt={5} activeStep={-1} orientation="vertical">
-        {puppetSteps[key].steps
-        .filter(f => !!f.label)
-        .map((step, index) => (
-          <Step key={step.label}>
-            <StepLabel>{step.label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-
+      <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="Steps" />
+        <Tab label="PuppetL" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Stepper key={key} mt={5} activeStep={-1} orientation="vertical">
+          {puppetSteps[key].steps
+          .filter(f => !!f.label)
+          .map((step, index) => (
+            <Step key={step.label}>
+              <StepLabel>{step.label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <pre>{JSON.stringify(puppetSteps, 0, 2)}</pre>
+      </TabPanel>
     </>) } 
     
     <Button sx={{mr: 1}} variant="outlined" onClick={() => {
       setPuppetSteps(null);
       onCancel && onCancel()
     }}>clear</Button>
-    
+
+    <Button sx={{mr: 1}} variant="outlined" onClick={() => {
+      setEditMode(s => !s); 
+    }}>edit</Button>
+    {editMode.toString()}
     <Button variant="contained" onClick={() => { 
       onSave && onSave(puppetSteps)
     }}>save</Button>
