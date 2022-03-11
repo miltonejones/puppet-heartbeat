ac2548311b8384db475e2fd3a81cb5cc0b861fdc
miltonejones <miltonejones@gmail.com>
2022-03-11 01:04:05 -0600

    updating layout more

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { Frame, Spacer, Flex } from './Control';
-import { Box, Divider, Typography, TextField } from '@mui/material';
+import { Box, Divider, Avatar, Typography, TextField , InputAdornment} from '@mui/material';
+import { Search }  from '@mui/icons-material';
 
 
 
@@ -8,6 +9,7 @@ export default function Layout ({ children, ...props}) {
     return (<>
     
     <Flex className="application-header">
+        <Avatar alt="logo" src="/icon.png" sx={{ width: 24, height: 24, mr: 1 }}/>
         <Box className="logo-font bold white" sx={{minWidth: 200}}>
             Puppeteer Studio
         </Box> 
@@ -15,12 +17,23 @@ export default function Layout ({ children, ...props}) {
         <TextField
             sx={{mr: 2, ml: 2}}
             classes={{root: 'input-outer'}}
-              InputProps={{ classes: {input: 'input-label'} }}  
+              InputProps={{
+              classes: {input: 'input-label'} ,
+              startAdornment: (
+                <InputAdornment sx={{color: 'beige'}} position="start">
+                  <Search />
+                </InputAdornment>
+              ), 
+            }}  
             fullWidth
             size="small" 
             placeholder="Search for tests by name or ARN"
+          
         />
-        <Box sx={{minWidth: 200}}>right menu</Box>
+        <Flex sx={{minWidth: 200 , color: '#989898'}}>
+            <Box className="dot" />
+            disconnected
+        </Flex>
     </Flex>
     <Frame offset={32} className="application-body">
         {children}
